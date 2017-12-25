a1916de414fc56a0ad18e2daf852b435fa84d11a
yuanmenghaixin <710584845@qq.com>
2017-12-25 17:42:14 +0800

    分支提交

--- gitTest1/src/main/java/classLoader/DemoObj.java
+++ gitTest1/src/main/java/classLoader/DemoObj.java
@@ -4,11 +4,17 @@ package classLoader;
  * Created by Peng.lv on 2017/6/28.
  */
 public class DemoObj {
+    public String toString3() {
+        return "I am DemoObj";
+    }
     @Override
     public String toString() {
         return "I am DemoObj";
     }
     public String toString1() {
+        return "I am DemoObj";
+    }
+    public String toString2() {
         return "I am DemoObj";
     }
 }
