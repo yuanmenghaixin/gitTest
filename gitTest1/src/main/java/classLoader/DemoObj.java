697fcfc2f5b2f9f92fbe558ae5d7fa0361573507
yuanmenghaixin <710584845@qq.com>
2017-12-25 18:28:08 +0800

    第7次提交dev分支

--- gitTest1/src/main/java/classLoader/DemoObj.java
+++ gitTest1/src/main/java/classLoader/DemoObj.java
@@ -4,21 +4,9 @@ package classLoader;
  * Created by Peng.lv on 2017/6/28.
  */
 public class DemoObj {
-    public String toString3() {
-        return "I am DemoObj";
-    }
     @Override
     public String toString() {
-        return "I am 23emoObj";
-    }
-    public String toString1() {
-        return "I sasasa DemoObj第六次";
+        return "I am DemoObj";
     }
 
-    public String toString5() {
-        return "分支第6次修改";
-    }
-    public String toString6() {
-        return "分支第五次修改";
-    }
 }
